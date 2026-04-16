9f3a7a424594a53407298bc23aca3afbbdb788d6
giamyiweijeremystudy <giamyiweijeremy.study@gmail.com>
2026-04-17 01:40:27 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Apply dark mode before first render to avoid flash
+const savedDark = localStorage.getItem('pt-dark-mode');
+if (savedDark === '1') document.documentElement.classList.add('dark');
+
 createRoot(document.getElementById("root")!).render(<App />);
